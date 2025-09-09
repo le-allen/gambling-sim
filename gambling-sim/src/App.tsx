741ac261de348b9086ee36e99d461eb2ff6412e9
13frog <139032644+le-allen@users.noreply.github.com>
2025-09-09 01:27:49 -0700

    added days

--- gambling-sim/src/App.tsx
+++ gambling-sim/src/App.tsx
@@ -3,14 +3,57 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const DaysOfWeek = {
+  Monday: "Monday",
+  Tuesday: "Tuesday",
+  Wednesday: "Wednesday",
+  Thursday: "Thursday",
+  Friday: "Friday",
+  Saturday: "Saturday",
+  Sunday: "Sunday"
+};
+
+type Day = typeof DaysOfWeek[keyof typeof DaysOfWeek];
+
 function App() {
   const [balance, setBalance] = useState(0);
   const [time, setTime] = useState(9);
+  const [day, setDay] = useState<Day>(DaysOfWeek.Monday);
+
+  const nextDay = () => {
+    switch(day) {
+      case DaysOfWeek.Monday:
+        setDay(DaysOfWeek.Tuesday);
+        break;
+      case DaysOfWeek.Tuesday:
+        setDay(DaysOfWeek.Wednesday);
+        break;
+      case DaysOfWeek.Wednesday:
+        setDay(DaysOfWeek.Thursday);
+        break;
+      case DaysOfWeek.Thursday:
+        setDay(DaysOfWeek.Friday);
+        break;
+      case DaysOfWeek.Friday:
+        setDay(DaysOfWeek.Saturday);
+        break;
+      case DaysOfWeek.Saturday:
+        setDay(DaysOfWeek.Sunday);
+        break;
+      case DaysOfWeek.Sunday:
+        setDay(DaysOfWeek.Monday);
+        break;
+    }
+  }
 
   const friesInTheBag = async () => {
     setBalance(balance + 10);
     setTime(time + 1);
-    if(time === 17) setTime(9);
+    if(time === 17) {
+      setTime(9);
+      nextDay()
+    };
+
   };
 
   return (
@@ -22,6 +65,7 @@ function App() {
         </button>
         <p>${balance}</p>
         <p>{time}:00</p>
+        <p>{day}</p>
       </div>
     </>
   )
